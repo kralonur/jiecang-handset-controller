c31d1d2a110c829357481e6a497f49f1f6af2a10
kralonur <kralonur1998@gmail.com>
2026-05-13 17:03:33 +0300

    feat: sending some commands with uart

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -13,6 +13,7 @@ use embassy_time::{Duration, Timer};
 use esp_backtrace as _;
 use esp_hal::clock::CpuClock;
 use esp_hal::timer::timg::TimerGroup;
+use esp_hal::uart::{Config as UartConfig, UartTx};
 use esp_println as _;
 
 // This creates a default app-descriptor required by the esp-idf bootloader.
@@ -57,10 +58,75 @@ async fn main(spawner: Spawner) -> ! {
     // TODO: Spawn some tasks
     let _ = spawner;
 
+    let mut handset_tx = UartTx::new(peripherals.UART1, UartConfig::default().with_baudrate(9_600))
+        .expect("failed to configure UART1")
+        .with_tx(peripherals.GPIO16);
+
+    let mut test_step = 0u16;
+    let mut refresh_count = 0u32;
+    let mut current_packet = test_packet(test_step);
+    log_test_step(test_step, current_packet);
+
     loop {
-        info!("Hello world!");
-        Timer::after(Duration::from_secs(1)).await;
+        let mut written = 0;
+        while written < current_packet.len() {
+            written += handset_tx.write(&current_packet[written..]).unwrap_or(0);
+        }
+        handset_tx.flush().ok();
+
+        refresh_count += 1;
+        if refresh_count >= 20 {
+            refresh_count = 0;
+            test_step = (test_step + 1) % TEST_STEP_COUNT;
+            current_packet = test_packet(test_step);
+            log_test_step(test_step, current_packet);
+        }
+
+        Timer::after(Duration::from_millis(50)).await;
     }
 
     // for inspiration have a look at the examples at https://github.com/esp-rs/esp-hal/tree/esp-hal-v1.1.0/examples
+}
+
+fn height_packet(height_mm: u16) -> [u8; 4] {
+    let [hi, lo] = height_mm.to_be_bytes();
+    [0x01, 0x01, hi, lo]
+}
+
+const ERROR_STEP_COUNT: u16 = 16;
+const HEIGHT_STEP_COUNT: u16 = 30;
+const TEST_STEP_COUNT: u16 = 1 + ERROR_STEP_COUNT + HEIGHT_STEP_COUNT;
+
+fn test_packet(step: u16) -> [u8; 4] {
+    if step == 0 {
+        return [0x01, 0x04, 0x01, 0xaa];
+    }
+
+    let error_step = step - 1;
+    if error_step < ERROR_STEP_COUNT {
+        return [0x01, 0x02, (error_step as u8) << 4, 0x00];
+    }
+
+    let height_step = error_step - ERROR_STEP_COUNT + 1;
+    height_packet(height_step * 10)
+}
+
+fn log_test_step(step: u16, packet: [u8; 4]) {
+    if step == 0 {
+        info!(
+            "test reset packet={:02x} {:02x} {:02x} {:02x}",
+            packet[0], packet[1], packet[2], packet[3]
+        );
+    } else if step <= ERROR_STEP_COUNT {
+        info!(
+            "test error arg0=0x{:02x} packet={:02x} {:02x} {:02x} {:02x}",
+            packet[2], packet[0], packet[1], packet[2], packet[3]
+        );
+    } else {
+        let height = (step - ERROR_STEP_COUNT) * 10;
+        info!(
+            "test height={} packet={:02x} {:02x} {:02x} {:02x}",
+            height, packet[0], packet[1], packet[2], packet[3]
+        );
+    }
 }
